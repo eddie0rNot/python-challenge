0bec40e94004a1146efca568c7819c7de626cf45
eddie0rNot <eddie.buckalew@gmail.com>
2021-01-16 13:37:23 -0600

    added code outline

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,8 @@
-
+#create script that analyzes budget_data.csv
+#calculate total number of months in dataset
+#calculate net total profit/loss over entire timeframe
+#calculate average profit/loss by period
+#calculate largest period increase, month and amount
+#calculate largest period decrease, month and amount
+#print results to terminal
+#export results as a text file
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,8 @@
-
+#create script that analyzes election_data.csv
+#calculate total number of votes cast
+#create complete list of all candidates who received votes
+#calculate percent of votes each candidate won
+#calculate total votes by candidate
+#determine winner of election based on popular vote
+#print results to terminal
+#export results as a text file
