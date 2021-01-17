2371e753f55cad14806533a7c1789ca28b447093
eddie0rNot <eddie.buckalew@gmail.com>
2021-01-17 15:18:17 -0600

    update average

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,22 @@ import os
 #import csv module
 import csv
 
+#import file
+csvpath = os.path.join('Resources','budget_data.csv')
+
+with open(csvpath) as csvfile:
+    datareader = csv.reader(csvfile,delimiter=",")
+    csv_header = next(csvfile)
+    profits = []
+    for data in datareader:
+        profits.append(int(data[1]))
+        def average(profits):
+            length = len(profits)
+            total = 0.0
+            for profit in profits:
+                total += profit
+            return total / length
+    print(float(average(profits)))
 
 #calculate total number of months in dataset
 #calculate net total profit/loss over entire timeframe
